13c889343ad332ac150b1dbeb47cc280d7df225c
widedboukouba <114486009+widedboukouba@users.noreply.github.com>
2023-11-21 14:23:03 +0100

    not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       
-      <img src="https://business.trustedshops.fr/hubfs/1-TS_B2B/content/FR/20220712-erreur-404/erreur-404-NL.jpg" alt="erreur page" style={{width:"100vw", paddingTop:"200px", marginRight:"200px"}}></img>
+      <img src="https://static.vecteezy.com/ti/vecteur-libre/p1/7956087-erreur-404-deconnexion-de-l-internet-indisponible-peu-de-gens-sont-en-colere-vecteur-illustrationle-plate-vectoriel.jpg" alt="erreur page" style={{width:"100vw", marginRight:"200px"}}></img>
     </div>
   );
 };
